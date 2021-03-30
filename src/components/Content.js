38df79be4cfbc091f59f2985d11a3d08518b86dc
pabloOcanto <pomalianni@gmail.com>
2021-03-29 22:52:18 -0300

    init

---
+++ src/components/Content.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Content = () =>(
+    <div  style={{display: "inline-block"}}>
+         <p>Inicio de Contenido</p>
+    </div>
+);
+
+export default Content;
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = () =>(
+    <div style={{backgroundColor: "blue"}}>
+    </div>
+);
+
+export default Header;
