ff9d799570edac021e523e839f8b5c50b106210f
pabloOcanto <pomalianni@gmail.com>
2021-05-09 21:13:11 -0300

    agregado de yup y formulario travel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import TravelForm from '../Form/TravelForm'
 
 class Home extends Component {
     render(){
         return (
             <div>
                 Home principal
+                <TravelForm />
             </div>
         )
     }
