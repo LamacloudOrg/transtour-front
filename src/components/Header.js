f02a4c21cfd3ada00e79f0c9145ebbf3ee5927a1
pabloOcanto <pomalianni@gmail.com>
2021-08-30 23:53:12 -0300

    agregado home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import  {withRouter  } from 'react-router-dom';
 
-const Header= () =>(
+class Header extends Component{
+
+
+  constructor(props) {
+    super(props);
+    this.handleClick.bind(this);
+    
+    }
+     
+    handleClick =()=>{
+
+        const { history } = this.props;
+        history.push("/home")
+    }
+
+    render() {
+
+    return(
 
     <div className="row">
-        <h1 className="text-center">Transtour Home Page</h1>
+        <h1 className="text-center" ><a href="#" onClick={()=>this.handleClick()}>Transtour Home Page</a></h1>
     </div>
-)
+    )
+    }
+}
 
-export default Header;
+export default withRouter(Header);
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
+import nostros from '../images/nosotros.jpg'
+
+const height="150px"
 
 const Home= () =>(
-    <div class="row">
-    <div className="page-footer font-small blue pt-4">
-    
-        <div className="text-center text-md-left">
-            <div className="col-md-6 mt-md-0 mt-3">
-            <h5 className="text-uppercase">Body Content</h5>
-            <p>Bienvenido a  Transtour</p>
-            </div>
-        </div>
-    
+    <div className="row">
+
+    <div className="col-6">
+    <div className="text-center">
+        <h4>Area de Administracion</h4>
+        <img src={nostros} alt="transtour-admin" />
+
      </div>
-     </div>   
-    );
+     </div>
+
+     <div className="col-6">
+
+    <div>
+        <h2>Bienvenidos a nuestra web! </h2>
+        <h3>Somos una empresa de traslados ejecutivos, con especialización en turismo Internacional.</h3>
+        <p>Brindamos servicios a Empresas, Agencias de turismo y Hoteles de primer nivel.<br/>
+            Los distintos traslados están controlados y monitoreados por una mesa de control operativa las 24 horas del día.<br/>
+            Contamos con Guías, coordinadores y conductores profesionales bilingües.<br/>
+            Esperamos que puedan contar con nosotros y experimentar la calidad de nuestros servicios.</p>
+    </div>
+
+    </div>
+
+
+    </div>
+
+
+);
 
 export default Home;
